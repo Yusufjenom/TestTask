dbf18f1b5e5556cb4abcae92b7abc495975f8db5
Yusufjenom <jenomy32@gmail.com>
2023-12-16 01:00:43 -0800

    final update to the update form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,18 @@ import { createBrowserRouter,
             Navigate
           } from 'react-router-dom';
 import RootLayout from './RootLayout';
-import Home from './Pages/Home';
+import Home from './Pages/Error';
 import SubmitForm from './Pages/SubmitForm';
 import EditForm from './Pages/EditForm';
 
-
+const available = JSON.parse(localStorage.getItem("editSafeHouse"));
+console.log(available)
 function App() {
       const router = createBrowserRouter(createRoutesFromElements(
         <Route path='/' element={<RootLayout/>}>
-            <Route index element={<Home/>}/>
-            <Route path='/submit' element={<SubmitForm/>}/>
+            <Route index element={<SubmitForm/>}/>
             <Route path='/update/:id' element={<EditForm/>}/>
+            <Route path='*' element={<Error/>}/>
         </Route>
       ))
   return (
